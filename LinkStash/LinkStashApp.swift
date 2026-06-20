a92b6fc68ddf14c232777a341a8c20ee2ab712f7
Prisha <prishaagarwalla50@gmail.com>
2026-06-19 23:54:58 -0500

    Initial Commit

---
+++ LinkStash/LinkStashApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkStashApp.swift
+//  LinkStash
+//
+//  Created by Prisha Agarwalla on 6/19/26.
+//
+
+import SwiftUI
+
+@main
+struct LinkStashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
